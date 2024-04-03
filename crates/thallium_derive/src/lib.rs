166447df90419973262cf0ca21aceff80dcfa7c6
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2024-04-03 15:11:05 +1300

    Removed `thallium_ecs` dependency from 	hallium_derive`

--- crates/thallium_derive/src/lib.rs
+++ crates/thallium_derive/src/lib.rs
@@ -4,7 +4,7 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-/// Derives the [`Component`](thallium_ecs::Component) trait
+/// Derives the [`thallium_ecs::Component`] trait
 #[proc_macro_derive(Component)]
 pub fn derive_component(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -13,13 +13,13 @@ pub fn derive_component(input: TokenStream) -> TokenStream {
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
     quote! {
-        impl #impl_generics ::thallium_ecs::Component for #name #ty_generics #where_clause {
+        impl #impl_generics ::thallium::ecs::Component for #name #ty_generics #where_clause {
         }
     }
     .into()
 }
 
-/// Derives the [`Resource`](thallium_ecs::Resource) trait
+/// Derives the [`thallium_ecs::Resource`] trait
 #[proc_macro_derive(Resource)]
 pub fn derive_resource(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -28,7 +28,7 @@ pub fn derive_resource(input: TokenStream) -> TokenStream {
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
     quote! {
-        impl #impl_generics ::thallium_ecs::Resource for #name #ty_generics #where_clause {
+        impl #impl_generics ::thallium::ecs::Resource for #name #ty_generics #where_clause {
         }
     }
     .into()
