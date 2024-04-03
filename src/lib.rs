96061cf465e3a5c5a03753c9a8fae52717aad1c1
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2024-04-03 15:36:49 +1300

    Updated docs, this is a test

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,20 @@
 
 /// An alias for [`thallium_ecs`], also with the related deives from [`thallium_derive`]
 #[cfg(feature = "ecs")]
+#[cfg_attr(docsrs, doc(cfg(feature = "ecs")))]
 pub mod ecs {
     #[cfg(feature = "derive")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "derive")))]
     pub use thallium_derive::{Component, Resource};
+
+    #[cfg_attr(docsrs, doc(cfg(feature = "ecs")))]
     pub use thallium_ecs::*;
 }
 
 /// An alias for [`thallium_derive`]
 #[cfg(feature = "derive")]
+#[cfg_attr(docsrs, doc(cfg(feature = "derive")))]
 pub mod derive {
+    #[cfg_attr(docsrs, doc(cfg(feature = "derive")))]
     pub use thallium_derive::*;
 }
