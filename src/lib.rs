e2f27a553e3dee010f617c628828ec8eaf497148
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2024-04-03 15:01:04 +1300

    Added features for ecs and derive

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 #![doc = include_str!("../README.md")]
 
 /// An alias for [`thallium_ecs`], also with the related deives from [`thallium_derive`]
+#[cfg(feature = "ecs")]
 pub mod ecs {
+    #[cfg(feature = "derive")]
     pub use thallium_derive::{Component, Resource};
     pub use thallium_ecs::*;
 }
 
 /// An alias for [`thallium_derive`]
+#[cfg(feature = "derive")]
 pub mod derive {
     pub use thallium_derive::*;
 }
