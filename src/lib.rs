a5e15330178fade892f7fb519a5d14161476b344
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2024-04-03 18:58:35 +1300

    Finally fixed it

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
-#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![doc = include_str!("../README.md")]
 
 /// An alias for [`thallium_ecs`], also with the related deives from [`thallium_derive`]
 #[cfg(feature = "ecs")]
-#[cfg_attr(docsrs, doc(cfg(feature = "ecs")))]
 pub mod ecs {
     #[cfg(feature = "derive")]
-    #[cfg_attr(docsrs, doc(cfg(feature = "derive")))]
     pub use thallium_derive::{Component, Resource};
 
     pub use thallium_ecs::*;
@@ -14,7 +12,6 @@ pub mod ecs {
 
 /// An alias for [`thallium_derive`]
 #[cfg(feature = "derive")]
-#[cfg_attr(docsrs, doc(cfg(feature = "derive")))]
 pub mod derive {
     pub use thallium_derive::*;
 }
